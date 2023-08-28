745bd25d614a70ac6ee00a833519fd4a66936bdf
Thiyaghessan <thiyaghessan@gmail.com>
2023-08-28 09:30:46 -0400

    Added Documentation describing function

---
+++ parse-ntee.R
@@ -0,0 +1,14 @@
+
+#' A complete function takes any user input values from the arguments:
+
+#'    ntee.group
+#'    ntee.code
+#'    ntee.orgtype
+
+#' And returns the correct list of NTEE codes that match the filter 
+#' requirements.
+
+#' It will also raise an error if the user tries argument values that are 
+#' undefined and print an informative message.
+
+
