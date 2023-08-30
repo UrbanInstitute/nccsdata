4ccbdfad2cd0a11ccc05c7b8682027d85acd748b
Thiyaghessan <thiyaghessan@gmail.com>
2023-08-30 09:16:53 -0400

    Started docstrings for function

---
+++ parse-ntee/generate_ntee_regex.R
@@ -0,0 +1,60 @@
+#' This function returns a regex query to parse NTEE2 Codes
+#' 
+#' @description This function creates the regex query that will be used
+#' to filter NTEE2 codes based on user inputs.
+#' 
+#' @param ntee.group character vector. Vector of desired Industry Group codes
+#'  to filter.
+#' @param ntee.code character vector. Sequence of desired Industry, Division and
+#' Subdivision codes (old code structure) to use in filtering.
+generate_ntee_regex <- function(ntee.group, ntee.code, ntee.orgtype){
+  # Formulate regex query based on user input
+  
+  level1_query <- ifelse(
+    ntee.group == "all",
+    "[A-Z][A-Z][A-Z]",
+    ntee.group
+  )
+  
+  level_2to4_query <- ifelse(
+    ntee.code == "all",
+    "[A-Z][0-9][A-Z0-9]",
+    
+    ifelse(
+      grepl("[A-Z]$", ntee.code) | grepl("[A-Z][xX][xX]$", ntee.code),
+      paste(substring(ntee.code, 1, 1), "[0-9][A-Z0-9]", sep = ""),
+      
+      ifelse(
+        grepl("[A-Z][0-9]$", ntee.code) | grepl("[A-Z][0-9][xX]$", ntee.code),
+        paste(substring(ntee.code, 1, 2), "[A-Z0-9]", sep = ""),
+        ntee.code
+      )
+    )
+  )
+  
+  level_5_query <- ifelse(
+    ntee.orgtype == "all",
+    "[A-Z][A-Z]",
+    ntee.orgtype
+  )
+  
+  full_query <- paste(
+    level1_query,
+    "-",
+    level_2to4_query,
+    "-",
+    level_5_query,
+    sep = "")
+  
+  
+  full_query <- data.table::CJ(
+    level1_query,
+    level_2to4_query,
+    level_5_query
+  )[, paste(level1_query,
+            level_2to4_query, 
+            level_5_query,
+            sep = "-")]
+  
+  return(full_query)
+}
