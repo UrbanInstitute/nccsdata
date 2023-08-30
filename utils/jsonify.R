90cdf1e1e1a841fc84ec6066eb23e7f30644aa05
Thiyaghessan <thiyaghessan@gmail.com>
2023-08-30 16:43:45 -0400

    Scripts with utility functions used across package

---
+++ utils/jsonify.R
@@ -0,0 +1,20 @@
+#' Function that prettifies json
+jsonify_f <- function(f)
+{
+  f <- as.factor(f)
+  f_level <- levels(f)
+  f_level <- gsub( "'", "", f_level ) # remove quotes
+  f_level <- gsub( '"', '', f_level ) # remove apostrophes
+  label <- f_level
+  d <- data.frame(f_level,label)
+  jd <- jsonlite::toJSON( d )
+  jd <- gsub( "\\{", "  \\{  ", jd )
+  jd <- gsub( ":", " :  ", jd )
+  jd <- gsub( '",', '"  ,  ', jd )
+  jd <- gsub( '","', '",  "', jd )
+  jd <- gsub( "\\},", "  \\}, \n", jd )
+  jd <- gsub( "\\[", "\\[ \n", jd )
+  jd <- gsub( "\\]", "\n\\]", jd )
+  jd <- gsub( "\\}\n", "  \\}\n", jd )
+  return(jd)
+}
