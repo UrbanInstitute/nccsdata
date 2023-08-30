b7843ad860b9945e54731490c0697a1b46459841
Thiyaghessan <thiyaghessan@gmail.com>
2023-08-30 09:09:21 -0400

    Function to generate level 3 and 4 of NTEE2 code

--- parse-ntee/get_ntee_level_3_4.R
+++ parse-ntee/get_ntee_level_3_4.R
@@ -1,53 +1,36 @@
-#' Script to generate regex queries for parsing population of NTEE2 codes.
+#' Function to generate level 3 and 4 of NTEE2 code
+#' 
+#' @description This function takes in digits23 and digits45 from the old
+#' NTEE codes to create levels 3 and 4 of NTEE2 code
+#' 
+#' @param digits23 character. Digits in the 2nd and 3rd place of old code
+#' @param digits45 character. Digits in the 4th and 5th place of old code
+#' 
+#' @usage get_ntee_level_3_4(digits23, digits45)
+#' 
+#' @return The level 3 and 4 codes concatenated together in a string
+#' 
+#' @examples 
+#' get_ntee_level_3_4("23", "45")
+#' get_ntee_level_3_4("03", "22")
+#' 
+#' @note 
+#' See https://github.com/Nonprofit-Open-Data-Collective/mission-taxonomies/blob/main/NTEE-disaggregated/README.md
+#' for more details
 
-generate_ntee_regex <- function(ntee.group, ntee.code, ntee.orgtype){
-  # Formulate regex query based on user input
+get_ntee_level_3_4 <- function(digits23, digits45){
   
-  level1_query <- ifelse(
-    ntee.group == "all",
-    "[A-Z][A-Z][A-Z]",
-    ntee.group
+  ntee2_level_3_4 <- ifelse(
+    as.numeric(digits23) > 19,
+    substring(digits23, 1, 2),
+    substring(digits45, 1, 2)
   )
   
-  level_2to4_query <- ifelse(
-    ntee.code == "all",
-    "[A-Z][0-9][A-Z0-9]",
-    
-    ifelse(
-      grepl("[A-Z]$", ntee.code) | grepl("[A-Z][xX][xX]$", ntee.code),
-      paste(substring(ntee.code, 1, 1), "[0-9][A-Z0-9]", sep = ""),
-      
-      ifelse(
-        grepl("[A-Z][0-9]$", ntee.code) | grepl("[A-Z][0-9][xX]$", ntee.code),
-        paste(substring(ntee.code, 1, 2), "[A-Z0-9]", sep = ""),
-        ntee.code
-      )
-    )
+  ntee2_level_3_4 <- ifelse(
+    is.na(ntee2_level_3_4),
+    substring(digits23, 1, 2),
+    ntee2_level_3_4
   )
   
-  level_5_query <- ifelse(
-    ntee.orgtype == "all",
-    "[A-Z][A-Z]",
-    ntee.orgtype
-  )
-  
-  full_query <- paste(
-    level1_query,
-    "-",
-    level_2to4_query,
-    "-",
-    level_5_query,
-    sep = "")
-  
-  
-  full_query <- data.table::CJ(
-    level1_query,
-    level_2to4_query,
-    level_5_query
-  )[, paste(level1_query,
-            level_2to4_query, 
-            level_5_query,
-            sep = "-")]
-  
-  return(full_query)
+  return(ntee2_level_3_4) 
 }
