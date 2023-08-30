b83a369f9f1358a3e9049dd338619827e8427500
Thiyaghessan <thiyaghessan@gmail.com>
2023-08-30 11:19:49 -0400

    Removed nested ifelse statements for readability

--- parse-ntee/validate_inp.R
+++ parse-ntee/validate_inp.R
@@ -28,34 +28,31 @@ validate_inp <- function(ntee.group,
                          ind_group_codes,
                          level_2_4_codes,
                          org_type_codes){
-  ifelse(
-    ! ntee.group %in% c(ind_group_codes, "all"),
+  
+  if (! ntee.group %in% c(ind_group_codes, "all")){
     stop("Invalid Industry Group \n 
           List of available groups can be found at: \n
-          https://github.com/Nonprofit-Open-Data-Collective/mission-taxonomies/blob/main/NTEE-disaggregated/README.md"),
+          https://github.com/Nonprofit-Open-Data-Collective/mission-taxonomies/blob/main/NTEE-disaggregated/README.md")
+  } else {
     message("Collecting Matching Industry Groups")
-  )
+  }
   
-  ifelse(
-    ! ntee.code %in% c(level_2_4_codes, "all"),
-    ifelse(
-      grepl("[A-Z][0-9xX]*[A-Z0-9xX]", ntee.code),
-      message("Collecting Matching Industry Division and Subdivisions"),
-      stop("Invalid Industry Division Subdivision Combination \n 
+  if (ntee.code %in% c(level_2_4_codes, "all") | 
+      grepl("[A-Z][0-9xX]*[A-Z0-9xX]", ntee.code)){
+    message("Collecting Matching Industry Division and Subdivisions")
+  } else {
+    stop("Invalid Industry Division Subdivision Combination \n 
           List of available Combinations can be found at: \n
           https://github.com/Nonprofit-Open-Data-Collective/mission-taxonomies/blob/main/NTEE-disaggregated/README.md")
-    ),
-    message("Collecting Matching Industry Division and Subdivisions")
-    
-  ) 
+  }
   
-  
-  ifelse(
-    ! ntee.orgtype %in% c(org_type_codes, "all"),
+  if (ntee.orgtype %in% c(org_type_codes, "all")) {
+    message("Collecting Matching Organization Types")
+  } else {
     stop("Invalid Organization Type \n 
           List of available Organization Types can be found at: \n
-          https://github.com/Nonprofit-Open-Data-Collective/mission-taxonomies/blob/main/NTEE-disaggregated/README.md"),
-    message("Collecting Matching Organization Types")
-  )
+          https://github.com/Nonprofit-Open-Data-Collective/mission-taxonomies/blob/main/NTEE-disaggregated/README.md")
+    
+  }
   
 }
