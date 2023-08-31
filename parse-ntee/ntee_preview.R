80a571e5d2bc19b95aad46282d1cf27f8ed5e44f
Thiyaghessan <thiyaghessan@gmail.com>
2023-08-31 15:02:53 -0400

    Finished MVP

--- parse-ntee/ntee_preview.R
+++ parse-ntee/ntee_preview.R
@@ -1,6 +1,28 @@
+source("parse_ntee.R")
+library("dplyr")
+library("reactable")
+
 #' Function to preview NTEE codes
 #' 
 
-ntee_preview <- function(){
+ntee_preview <- function(ntee.group = "all",
+                         ntee.code = "all",
+                         ntee.orgtype = "all",
+                         visualize = FALSE){
+  
+  ntee2_codes <- parse_ntee(ntee.group = ntee.group,
+                            ntee.code = ntee.code,
+                            ntee.orgtype = ntee.orgtype)
+  
+  ntee_df <- readRDS("../data-raw/ntee_df.RDS")  
+  
+  filtered_df <- ntee_df %>% 
+    dplyr::filter(ntee2.code %in% ntee2_codes[[1]])
+  
+  if (visualize == FALSE){
+    return(filtered_df)
+  } else {
+    return(reactable(filtered_df))
+  }
   
 }
