10235406337689b83fc8d539ee6540a04cf059fb
Thiyaghessan <thiyaghessan@gmail.com>
2023-08-31 10:51:03 -0400

    Renamed columns according to snakeCase and allowing for linkage with tract data. Also saved processed dataframe as RDS

--- cbsa-table-create/cbsa_table_create.R
+++ cbsa-table-create/cbsa_table_create.R
@@ -15,19 +15,24 @@ cbsa_table_create <- function(path_to_raw_cbsa = "cbsa2fipsxw.csv"){
   
   # Preprocess Columns
   cbsa_df <- cbsa_df %>% 
-    rename(id = cbsacode,
-           metropolitan_and_division_code = metropolitandivisioncode,
-           csa_code = csacode,
-           cbsa_title = cbsatitle,
-           metropolitan_micropolitan_statis = metropolitanmicropolitanstatis,
-           metropolitan_division_title = metropolitandivisiontitle,
-           csa_title = csatitle,
-           county_county_equivalent = countycountyequivalent,
-           state_name = statename,
-           fips_state_code = fipsstatecode,
-           fips_county_code = fipscountycode,
-           central_outlying_county = centraloutlyingcounty) %>% 
-    mutate(state_abbr = usdata::state2abbr(state_name))
+    rename(metro.census.cbsa.geoid = cbsacode,
+           metro.div.geoid = metropolitandivisioncode,
+           metro.census.csa.geoid = csacode,
+           metro.census.cbsa.name = cbsatitle,
+           metro.micro.name = metropolitanmicropolitanstatis,
+           metro.div.name = metropolitandivisiontitle,
+           metro.census.csa.name = csatitle,
+           census.county.name = countycountyequivalent,
+           state.census.name = statename,
+           state.censu.geoid = fipsstatecode,
+           census.county.geoid = fipscountycode,
+           census.centrout.name = centraloutlyingcounty) %>% 
+    mutate(state.census.abbr = usdata::state2abbr(state.census.name))
+  
+  # Save as RDS
+  
+  saveRDS(cbsa_df, "cbsa_df.RDS")
+  message("Dataframe preprocessed and saved as .RDS")
   
   # Nest dataframe by cbsacode and write to json
   
