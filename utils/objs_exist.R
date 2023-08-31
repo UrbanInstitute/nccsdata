4593b5529ef0c984da26ef4b44f714cd70d5b055
Thiyaghessan <thiyaghessan@gmail.com>
2023-08-31 08:43:16 -0400

    Helper function to check if multiple objects exist in memory

---
+++ utils/objs_exist.R
@@ -0,0 +1,18 @@
+#' Check if ALL objects named one vector exist in memory
+#' 
+#' @description This function is used to check if both block and tract data
+#' tables exist in memory
+#' 
+#' @param ... vector of objects.
+#' 
+#' @usage objs_exist(c("block_dt", "tract_dt"))
+#' 
+#' @return A single boolean value indicating whether all objects are present 
+#' in memory
+
+objs_exist() <- function(...) {
+  
+  ls <- list(...)
+  all(sapply(ls, exists))
+  
+}
