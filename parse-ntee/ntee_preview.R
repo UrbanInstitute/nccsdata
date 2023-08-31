747e6f8a5f1087b5786be1e467f8528ce094fe10
Thiyaghessan <thiyaghessan@gmail.com>
2023-08-31 15:57:06 -0400

    Finished docstrings

--- parse-ntee/ntee_preview.R
+++ parse-ntee/ntee_preview.R
@@ -4,8 +4,36 @@ library("reactable")
 
 #' Function to interactively explore NTEE codes
 #' 
-#' @description 
+#' @description This function takes user inputs across levels 1- 5 of the
+#' NTEE2 code structure and returns selected columns from the NTEE database.
+#' The use can choose to return a dataframe or reactable object.
 #' 
+#' @param ntee.group character vector. Vector of desired Industry Group codes
+#'  to filter. Use "all" to include all possible codes.
+#' 
+#' @param ntee.code character vector. Sequence of desired Industry, Division and
+#' Subdivision codes (old code structure) to use in filtering. Use "all" to 
+#' include all possible codes. Can also provide only partial codes. For example
+#' "A" or "Axx" will query NTEE2 codes based on Industry group "A" and all
+#' division and subdivisions.
+#' 
+#' @param ntee.orgtype character vector. Vector of Organization Types.
+#' Use "all" to include all possible codes.
+#' 
+#' @param cols character vector. A vector containing list of columns to select.
+#' Default value is "all" for all columns.
+#' 
+#' @param visualize boolean. A boolean value indicating whether to return a 
+#' reactable object or data frame.
+#' 
+#' @usage ntee_preview(ntee.group, ntee.code, ntee.orgtype, cols, visualize)
+#' 
+#' @returns a dataframe or reactable object with rows that match user arguments.
+#' 
+#' @examples 
+#' ntee_preview("all", "A1x", "all", "all",TRUE)
+#' ntee_preview("ART", "Axx", "all", "all",TRUE)
+#' ntee_preview("all", "Axx", "all", c("type.org", "univ"), FALSE)
 
 ntee_preview <- function(ntee.group = "all",
                          ntee.code = "all",
