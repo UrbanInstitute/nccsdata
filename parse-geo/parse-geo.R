b42d3f09cca2e3439175d6e3950fe708a4ebee0a
Thiyaghessan <thiyaghessan@gmail.com>
2023-08-31 08:46:05 -0400

    Moved helper functions to seperate scripts

--- parse-geo/parse-geo.R
+++ parse-geo/parse-geo.R
@@ -9,77 +9,8 @@ library("stringr")
 library("aws.s3")
 library("docstring")
 library("roxygen2")
-
-geo_preproc <- function(
-    block_s3_url = "s3://nccsdata/geo/xwalk_geoid/block_crosswalk.csv",
-    tract_s3_url = "s3://nccsdata/geo/xwalk_geoid/tract_crosswalk.csv"
-){
-  #' Preprocess Census Block and Tract Crosswalks
-  #' 
-  #' @description This function downloads the crosswalk datasets from an S3
-  #' bucket, reads them as data.tables, reformats the column names, and creates
-  #' a new column with state abbreviations for the Tract dataset.
-  #' 
-  #' @usage geo_preproc()
-  #' 
-  #' @return A string message indicating that the processed data.tables
-  #' are available in memory
-  #' 
-  #' @note Can also be used to recreate the data.tables if files get corrupted.
-  
-  message("Loading Datasets")
-  
-  # Load Data from S3
-  
-  block_dt <- 
-    save_object(block_s3_url) %>%
-    data.table::fread()
-  
-  tract_dt <- 
-    save_object(tract_s3_url) %>%
-    data.table::fread()
-  
-  # Rename columns and create state abbreviations in tract data.table
-  tract_dt <- tract_dt %>% 
-    dplyr::rename_all(
-      list(
-        ~ paste0("geo.", .)
-      )
-    ) %>% 
-    dplyr::mutate(geo.state = usdata::state2abbr(geo.state_name))
-  
-  # Rename columns in block data.table
-  block_dt <- block_dt %>% 
-    dplyr::rename_all(
-      ~ stringr::str_replace_all(., "_geoid", "")
-    ) %>% 
-    dplyr::rename_all(
-      ~ paste0("geo.", .)
-    )
-  
-  return("Data Loaded")
-  
-}
-
-exists.m <- function(...) {
-  
-  #' Check if ALL elements of one vector exist in another vector
-  #' 
-  #' @description This function is used in the validate_arg() function to 
-  #' ensure that the arguments supplied to parse_geo() are valid column names
-  #' in either the block or tract data.table objects.
-  #' 
-  #' @usage exists.m(vector_1, vector_2)
-  #' 
-  #' @return A single boolean value indicating whether all elements of vector_1
-  #' are present in vector_2
-  #' 
-  #' @examples 
-  #' exists.m(c("col_1", "col_2"), c("col_1", "col_2", "col_3"))
-
-  ls <- list(...)
-  all(sapply(ls, exists))
-}
+source("geo_data_get.R")
+source("../utils/objs_exist.R")
 
 parse_geo <- function(geo.level, ...){
   
