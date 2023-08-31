137718a3d5435ab6cc4e6f4b6730aa80abda3365
Thiyaghessan <thiyaghessan@gmail.com>
2023-08-31 09:05:35 -0400

    Documented @param names

--- parse-geo/parse-geo.R
+++ parse-geo/parse-geo.R
@@ -1,5 +1,4 @@
 ## Script to return Block or Tract IDs from Census tables that match
-
 ## Load required packages
 library("data.table")
 library("dplyr")
@@ -12,26 +11,41 @@ library("roxygen2")
 source("geo_data_get.R")
 source("../utils/objs_exist.R")
 
+#' Function that returns FIPS codes that match dynamic User arguments
+#' 
+#' @description Filters either the Block or Tract data.tables to return
+#' a list of FIPS codes that match conditions specified by the User
+#' 
+#' Parameters for the Block Tract
+#' 
+#' @param census.level string. data.table to parse; "BLOCK" | "TRACT"
+#' @param block.census.geoid string or vector. Vector of Block IDs (FIPS)
+#' @param tract.census.geoid string or vector. Vector of Tract IDs (FIPS)
+#' @param zcta.census.geoid string or vector. Vector of county IDs
+#' @param place.census.geoid string or vector. Vector of census place IDs
+#' @param county.census.geoid string or vector. Vector of County IDs
+#' @param vtd.census.geoid string or vector. Vector of Voting District IDs
+#' @param urbanrural.census.geoid string or vector. Vector of ZCTA IDs
+#' @param urbanrural.nces.geoid string or vector. Vector of NCES Locale IDs
+#' 
+#' Parameters for the Census Tract
+#' 
+#' @param tract.census.geoid string or vector. Vector of Tract IDs (FIPS) 
+#' @param county.census.geoid string or vector. Vector of County IDs
+#' @param puma.census.geoid string or vector. Vector of PUMA IDs
+#' @param state.census.geoid string or vector. Vector of state IDs
+#' @param state.census.name string or vector. Vector of state names
+#' @param metro.census.cbsa.geoid string or vector. Vector of cbsa IDs
+#' @param metro.census.cbsa.name string or vector. Vector of census area names
+#' @param metro.census.csa.geoid string or vector. Vector of csa IDs
+#' @param metro.census.csa.name string or vector. Vector of csa names
+#' @param region.woodard.nation string or vector. Vector of region names
+#' @param region.woodard.culture string or vector. Vector of culture labels
+#' @param region.census.main string or vector. Vector of region names
+#' @param region.census.division string or vector. Vector of subregion names
+#' @param state.census.abbr string or vector. Vector of state abbreviations
+
 parse_geo <- function(geo.level, ...){
-  
-  #' Function that returns FIPS codes that match dynamic User arguments
-  #' 
-  #' @description Filters either the Block or Tract data.tables to return
-  #' a list of FIPS codes that match conditions specified by the User
-  #' 
-  #' Parameters for the Block Tract
-  #' 
-  #' @param geo.level character. data.table to parse; "BLOCK" | "TRACT"
-  #' @param geo.block string or vector. Vector of Block IDs (FIPS)
-  #' @param geo.tract string or vector. Vector of Tract IDs (FIPS)
-  #' @param geo.county string or vector. Vector of county IDs
-  #' @param geo.place string or vector. Vector of census place IDs
-  #' @param geo.ua string or vector. Vector of Urban Area IDs
-  #' @param geo.vtd string or vector. Vector of Voting District IDs
-  #' @param geo.zcta string or vector. Vector of ZCTA IDs
-  #' @param geo.locale string or vector. Vector of NCES Locale IDs
-  #' 
-  #' Parameters for the Census Tract
   
   # Check if data is already preloaded
   if (exists.m("block_dt", "tract_dt")){
