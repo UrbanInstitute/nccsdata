5350f444359259bf6732f698be15bef89429b141
Thiyaghessan <thiyaghessan@gmail.com>
2023-09-01 13:16:35 -0400

    Moved jsonify function to utils

---
+++ R/utils.R
@@ -0,0 +1,25 @@
+#' Script with utility functions
+#' 
+#' Function that prettifies json
+#' 
+#' @import jsonlite
+
+jsonify_f <- function(f)
+{
+  f <- as.factor(f)
+  f_level <- levels(f)
+  f_level <- gsub( "'", "", f_level ) # remove quotes
+  f_level <- gsub( '"', '', f_level ) # remove apostrophes
+  label <- f_level
+  d <- data.frame(f_level,label)
+  jd <- jsonlite::toJSON( d )
+  jd <- gsub( "\\{", "  \\{  ", jd )
+  jd <- gsub( ":", " :  ", jd )
+  jd <- gsub( '",', '"  ,  ', jd )
+  jd <- gsub( '","', '",  "', jd )
+  jd <- gsub( "\\},", "  \\}, \n", jd )
+  jd <- gsub( "\\[", "\\[ \n", jd )
+  jd <- gsub( "\\]", "\n\\]", jd )
+  jd <- gsub( "\\}\n", "  \\}\n", jd )
+  return(jd)
+}
