c866ad8e295f81baa25ae88de0cf1cb481b5341c
Thiyaghessan <thiyaghessan@gmail.com>
2023-09-01 14:22:21 -0400

    Added parse_ntee_regex() function

--- R/ntee_helper.R
+++ R/ntee_helper.R
@@ -171,3 +171,30 @@ get_ntee_level_3_4 <- function(digits23, digits45){
   
   return(ntee2_level_3_4) 
 }
+
+#' Function to loop through and execute a vector of regex queries
+#' on a vector of ntee codes
+#' 
+#' @description This function executes a vector of regex queries on 
+#' a vector containing the population of NTEE2 codes
+#' 
+#' @param regexp_vec character vector. Vector containing all regex queries
+#' @param ntee_codes character vector. Vector containing population of NTEE2
+#' codes
+#' 
+#' @usage parse_ntee_regex(regexp_vec, ntee_codes)
+#' 
+#' @return vector of matched NTEE2 codes
+
+parse_ntee_regex <- function(regexp_vec, ntee_codes){
+  
+  matched_codes <- c()
+  
+  for (regexp in regexp_vec){
+    results <- ntee_codes[grep(regexp, ntee_codes)]
+    matched_codes <- c(results, matched_codes)
+  }
+  
+  return(unique(matched_codes))
+  
+}
