dad4b3c6f110fab3f3d0f6d77492fe29a3269adb
Thiyaghessan <thiyaghessan@gmail.com>
2023-09-05 13:51:58 -0400

    load data.table for ntee from correct path

--- nccsdata/R/get_data.R
+++ nccsdata/R/get_data.R
@@ -41,7 +41,8 @@ get_data <- function(ntee.level1 = "all",
   tinybmf_dat <- data.table::setDT(tinybmf)
   tract_dat <- data.table::setDT(tract_dat)
   block_dat <- data.table::setDT(block_dat)
-  ntee_dat <- data.table::setDT(load("data/ntee_df.rda"))
+  ntee_dat <- load("data/ntee_df.rda")
+  ntee_dat <- data.table::setDT(ntee_dat)
   cbsa_dat <- data.table::setDT(cbsa_df)
 
   # rename columns, and wrangle data
