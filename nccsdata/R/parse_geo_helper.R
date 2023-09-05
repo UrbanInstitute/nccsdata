06defd6db4ade538b234201a1b9f9a7166f86d5b
Thiyaghessan <thiyaghessan@gmail.com>
2023-09-05 08:26:18 -0400

    Moved geo_data_get() that downloads block and tract data from S3 and saves to disk

---
+++ nccsdata/R/parse_geo_helper.R
@@ -0,0 +1,46 @@
+#' Preprocess Census Block and Tract Crosswalks
+#'
+#' @description This function downloads the crosswalk datasets from an S3
+#' bucket, reads them as data.tables, reformats the column names, and creates
+#' a new column with state abbreviations for the Tract dataset.
+#'
+#' @param block_s3_url string. Path to S3 bucket with block crosswalk.
+#' @param tract_s3_url string. Path to S3 bucket with tract crosswalk.
+#'
+#' @usage geo_data_get()
+#'
+#' @return A string message indicating that the processed data.tables
+#' are available in memory
+#'
+#' @note Can also be used to recreate the data.tables if files get corrupted.
+
+geo_data_get <- function(
+    block_s3_url = "s3://nccsdata/geo/xwalk/BLOCKX.csv",
+    tract_s3_url = "s3://nccsdata/geo/xwalk/TRACTX.csv"
+){
+
+  message("Loading Datasets")
+
+  # Load Data from S3
+
+  block_dat <- save_object(block_s3_url) %>%
+    data.table::fread()
+
+  tract_dat <- save_object(tract_s3_url) %>%
+    data.table::fread()
+
+  # Create state abbreviations in tract data.table
+  tract_dat <- tract_dat %>%
+    dplyr::rename(metro.census.cbsa.geoid = metro.census.cbsa10.geoid,
+                  metro.census.cbsa.name = metro.census.cbsa10.name,
+                  metro.census.csa.geoid = metro.census.csa10.geoid,
+                  metro.census.csa.name = metro.census.csa10.name) %>%
+    dplyr::mutate(state.census.abbr = usdata::state2abbr(state.census.name))
+
+  # Save data as RDS
+  saveRDS(tract_dat, "tract_dat.RDS")
+  saveRDS(block_dat, "block_dat.RDS")
+
+  return("Data Loaded")
+
+}
