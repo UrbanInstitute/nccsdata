6cc01172de177bd2fddb03136afa2a281dbdee59
Thiyaghessan <thiyaghessan@gmail.com>
2023-09-05 10:47:18 -0400

    added roxygen skeleton for get_data()

---
+++ nccsdata/R/get_data.R
@@ -0,0 +1,29 @@
+#' @title FUNCTION_TITLE
+#' @description FUNCTION_DESCRIPTION
+#' @param formula PARAM_DESCRIPTION
+#' @param data PARAM_DESCRIPTION
+#' @param subset PARAM_DESCRIPTION
+#' @param weights PARAM_DESCRIPTION
+#' @param na.action PARAM_DESCRIPTION
+#' @param method PARAM_DESCRIPTION, Default: 'qr'
+#' @param model PARAM_DESCRIPTION, Default: TRUE
+#' @param x PARAM_DESCRIPTION, Default: FALSE
+#' @param y PARAM_DESCRIPTION, Default: FALSE
+#' @param qr PARAM_DESCRIPTION, Default: TRUE
+#' @param singular.ok PARAM_DESCRIPTION, Default: TRUE
+#' @param contrasts PARAM_DESCRIPTION, Default: NULL
+#' @param offset PARAM_DESCRIPTION
+#' @param ... PARAM_DESCRIPTION
+#' @return OUTPUT_DESCRIPTION
+#' @details DETAILS
+#' @examples
+#' \dontrun{
+#' if(interactive()){
+#'  #EXAMPLE1
+#'  }
+#' }
+#' @seealso
+#'  \code{\link[stats]{model.frame}}
+#' @rdname lm
+#' @export
+#' @importFrom stats model.frame
