f0dd5686b70e7e0f0320ff037723911d1fe68143
Thiyaghessan <thiyaghessan@gmail.com>
2023-09-05 18:54:31 -0400

    read processed ntee data from package files

--- nccsdata/R/ntee_main.R
+++ nccsdata/R/ntee_main.R
@@ -114,16 +114,14 @@ ntee_preview <- function(ntee.group = "all",
 
 parse_ntee <- function(ntee.group, ntee.code, ntee.orgtype){
 
-  ntee_code_ls <- ntee_preproc()
-
   # Validate user inputs
   validate_inp(
     ntee.group = ntee.group,
     ntee.code = ntee.code,
     ntee.orgtype = ntee.orgtype,
-    ind_group_codes = ntee_code_ls[[2]],
-    level_2_4_codes = ntee_code_ls[[3]],
-    org_type_codes = ntee_code_ls[[4]]
+    ind_group_codes = ntee_df$broad.category,
+    level_2_4_codes = ntee_df$old.code,
+    org_type_codes = ntee_df$type.org
   )
 
   # Generate regex queries if inputs are valid
@@ -136,7 +134,7 @@ parse_ntee <- function(ntee.group, ntee.code, ntee.orgtype){
   # Execute regex queries
   ntee2_codes <- parse_ntee_regex(
     regexp_vec = regex_queries,
-    ntee_codes = ntee_code_ls[[1]]
+    ntee_codes = ntee_df$ntee2.code
   )
 
   # Return NTEE2 Codes
