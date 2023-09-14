ca0b5bd28e673b72c804064616058be5bdd1dab7
Thiyaghessan <thiyaghessan@gmail.com>
2023-09-14 14:50:03 -0400

    validate time input

--- R/get_data_helpers.R
+++ R/get_data_helpers.R
@@ -7,9 +7,14 @@ validate_get_data <- function(dsname = NULL,
                               geo = NULL){
 
   stopifnot(
-    "Invalid datatype. dsname must be a string" =
+    "Invalid datatype. dsname must be a character scalar." =
       is_scalar_character(dsname) == TRUE,
     "Invalid dataseries. Select either 'core' or 'bmf'." =
-      (dsname == "core" | dsname == "bmf")
+      (dsname == "core" | dsname == "bmf"),
+    "Invalid datatype. time must be a character vector." =
+      is_character(time) == TRUE,
+    "Invalid date range specified. Choose years from 1989-2022." =
+      all(as.numeric(time) %in% seq(1989, 2022))
+
     )
 }
