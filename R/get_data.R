0f1b07638b27e85cf4df0a67c09a59dbec7b1e70
Thiyaghessan <thiyaghessan@gmail.com>
2023-09-15 08:52:57 -0400

    Added validation message end emptied function body for testing

--- R/get_data.R
+++ R/get_data.R
@@ -37,10 +37,11 @@ get_data <- function(dsname = NULL,
                      scope.formtype = NULL){
 
   # Validate inputs
-  validate_get_data(dsname = dsname,
-                    time = time,
-                    scope.orgtype = scope.orgtype,
-                    scope.formtype = scope.formtype)
+  valid_msg <- validate_get_data(dsname = dsname,
+                                 time = time,
+                                 scope.orgtype = scope.orgtype,
+                                 scope.formtype = scope.formtype)
+  message(valid_msg)
 
   # load in datasets as data.table
   tract_dat <- data.table::setDT(tract_dat)
@@ -48,75 +49,7 @@ get_data <- function(dsname = NULL,
   ntee_dat <- data.table::setDT(ntee_df)
   cbsa_dat <- data.table::setDT(cbsa_df)
 
-  # Data wrangling
-
-  # bmf data
-
-  tinybmf_dat <- tinybmf_dat %>%
-    dplyr::rename("tract.census.geoid" = .data$TRACT.GEOID.10,
-                  "block.census.geoid" = .data$BLOCK.GEOID.10,
-                  "state.census.abbr" = .data$STATE,
-                  "ntee2.code" = .data$NTEE2) %>%
-    dplyr::mutate(across(c("tract.census.geoid", "block.census.geoid"),
-                         stringr::str_replace,
-                         "GEO-",
-                         ""))
-  # cbsa data
-
-  cbsa_ex_cols <- setdiff(colnames(cbsa_df), colnames(tract_dat))
-  cbsa_dat <- cbsa_dat %>%
-    dplyr::select(append(.data$metro.census.cbsa.geoid, cbsa_ex_cols)) %>%
-    group_by(.data$metro.census.cbsa.geoid)
-
-  # Apply NTEE filters
-  if (any(! ntee.level1 == "all" | ! ntee.level2 == "all")){
-  ntee2_codes <- parse_ntee(ntee.group = ntee.level1,
-                            ntee.code = ntee.level2,
-                            ntee.orgtype = "all")
-  tinybmf_subset <- tinybmf_dat %>%
-    dplyr::filter(.data$ntee2.code %in% ntee2_codes) %>%
-    dplyr::left_join(ntee_dat, by = "ntee2.code")
-  } else {
-    tinybmf_subset <- tinybmf_dat %>%
-      dplyr::left_join(ntee_dat, by = "ntee2.code")
-  }
-
-  # Apply geographic filters
-
-
-  # State filter
-  if (! is.null(geo.state)) {
-    tinybmf_subset <- tinybmf_subset %>%
-      dplyr::filter(.data$state.census.abbr %in% geo.state)
-  }
-
-  # Census level
-  if (geo.level == "tract"){
-    tinybmf_subset <- tinybmf_subset %>%
-      dplyr::left_join(tract_dat, by = "tract.census.geoid")
-  } else if (geo.level == "block") {
-    tinybmf_subset <- tinybmf_subset %>%
-      dplyr::left_join(block_dat, by = "block.census.geoid")
-  } else if (geo.level == "both") {
-    tinybmf_subset <- tinybmf_subset %>%
-      dplyr::left_join(tract_dat, by = "tract.census.geoid") %>%
-      dplyr::left_join(block_dat, by = "block.census.geoid")
-  } else {
-    stop("Invalid geo.level, select either 'block', 'tract' or 'both'")
-  }
-
-  # CBSA filter
-  if (! is.null(geo.metro)){
-    tinybmf_subset <- tinybmf_subset %>%
-      dplyr::left_join(tract_dat, by = "tract.census.geoid") %>%
-      dplyr::select(.data$metro.census.cbsa.geoid %in% geo.metro) %>%
-      dplyr::group_by("metro.census.cbsa.geoid") %>%
-      dplyr::left_join(cbsa_dat, by = "metro.census.cbsa.geoid") %>%
-      dplyr::ungroup()
-  }
-
-
-  return(tinybmf_subset)
+  return("test")
 
 }
 
