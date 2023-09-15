bd9fb60c76106fb0317870f7cc935f192d2421bb
Thiyaghessan <thiyaghessan@gmail.com>
2023-09-15 08:45:36 -0400

    roxygen2 documentation

--- R/get_data_helpers.R
+++ R/get_data_helpers.R
@@ -2,6 +2,24 @@
 #'
 #' @description This function validates each argument passed to get_data() and
 #' returns informative error messages if user inputs are incorrect.
+#'
+#' @param dsname character scalar. Name of data series to query from S3.
+#' Valid inputs are "core" and "bmf", not both.
+#' @param time character vector. Dates of core/bmf files to query. Valid
+#' inputs range from 1989-2022. Default value is "current" for 2022.
+#' @param scope.orgtype character scalar. Organization type to query from
+#' core/bmf s3 bucket. Valid inputs are 'CHARITIES' for charities (501C3-PC),
+#' 'PRIVFOUND' for private foundations (501C3-PF) and 'NONPROFIT' for all
+#' nonprofits (501CE)
+#' @param scope.formtype character scalar. Form type to query from core/bmf s3
+#' bucket. Valid inputs are 'PC'(nonprofits that file the full version),
+#' 'EZ'(nonprofits that file 990EZs only), '
+#' PZ'(nonprofits that file both PC and EZ), or 'PF'(private foundations).
+#'
+#' @return message describing the data that is being queried.
+#'
+#' @usage validate_get_data(dsname, time, scope.orgtype, scope.formtype)
+#'
 #' @export
 validate_get_data <- function(dsname = NULL,
                               time = "current",
@@ -25,7 +43,7 @@ validate_get_data <- function(dsname = NULL,
     "Invalid datatype. scope.orgtype must be a string." =
       is_scalar_character(scope.orgtype) == TRUE,
 
-    "Invalid organisation type. Select 'CHARITIES' for charities (501C3-PC), 'PRIVFOUND' for private foundations (501C3-PF) and 'NONPROFIT' for all nonprofits (501CE) " =
+    "Invalid organisation type. Select 'CHARITIES' for charities (501C3-PC), 'PRIVFOUND' for private foundations (501C3-PF) and 'NONPROFIT' for all nonprofits (501CE)" =
       (scope.orgtype == "CHARITIES" |
        scope.orgtype == "PRIVFOUND" |
        scope.orgtype == "NONPROFIT"),
@@ -39,4 +57,6 @@ validate_get_data <- function(dsname = NULL,
        scope.formtype == "PZ" |
        scope.formtype == "PF")
     )
+
+  return("Valid inputs detected. Retrieving data.")
 }
