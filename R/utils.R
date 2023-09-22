ba9c04029c54e9c23ce76cd4488507dee1714da1
Thiyaghessan <thiyaghessan@gmail.com>
2023-09-22 09:43:21 -0400

    function to load data.table from s3 bucket

--- R/utils.R
+++ R/utils.R
@@ -108,7 +108,7 @@ obj_validate <- function(dsname,
 #' @importFrom dplyr %>%
 
 load_dt <- function(url){
-  df <- readr::read_csv(
+  dt <- readr::read_csv(
     url,
     col_types = readr::cols(default = "?",
                             FIPS = "i")
