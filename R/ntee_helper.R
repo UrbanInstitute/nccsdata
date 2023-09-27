b9f822ecde73ccf144da2f52ac9fe03fc6071563
Thiyaghessan <thiyaghessan@gmail.com>
2023-09-26 20:56:48 -0400

    changed functions to internal functions

--- R/ntee_helper.R
+++ R/ntee_helper.R
@@ -24,8 +24,6 @@
 #' generate_ntee_regex("ART", "A23", "RG")
 #' generate_ntee_regex("all", "Axx", "RG")
 #' generate_ntee_regex("EDU", "B", "all")
-#'
-#' @export
 
 generate_ntee_regex <- function(ntee.group, ntee.code, ntee.orgtype){
   # Formulate regex query based on user input
@@ -89,8 +87,6 @@ generate_ntee_regex <- function(ntee.group, ntee.code, ntee.orgtype){
 #' @note
 #' See https://github.com/Nonprofit-Open-Data-Collective/mission-taxonomies/blob/main/NTEE-disaggregated/README.md
 #' for more details
-#'
-#' @export
 
 get_ntee_level_3_4 <- function(digits23, digits45){
 
