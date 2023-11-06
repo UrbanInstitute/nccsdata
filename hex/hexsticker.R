fdba877949bf91d255823a4817459dcfaf536931
Jesse Lecy <jdlecy@gmail.com>
2023-11-06 16:56:42 -0700

    add hex icon script and files

---
+++ hex/hexsticker.R
@@ -0,0 +1,11 @@
+library(hexSticker)
+
+sticker( 
+  "nccs-gray2.png", 
+  package="nccsdata",
+  h_fill="#1275a3ff", 
+  h_color="#FFFFFF", 
+  p_size=25, p_color="#FFFFFF",
+  s_x=1, s_y=.80, 
+  s_width=.5, 
+  filename="hex.png" )
