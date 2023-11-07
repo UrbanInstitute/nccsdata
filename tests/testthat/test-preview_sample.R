52932001762599ddecff29cd1c76e724e3cba1ee
Thiyaghessan <thiyaghessan@gmail.com>
2023-11-07 16:10:58 -0500

    Add unit tests for preview_sample()
    Download a random core dataset and summarise a random subset of
    variables with preview_sample(). This serves as a check that the
    function works correctly.

---
+++ tests/testthat/test-preview_sample.R
@@ -0,0 +1,26 @@
+library(dplyr)
+
+
+
+for (i in 1:3){
+
+  test_data <- create_test() %>%
+    dplyr::slice_head(n = 1000)
+
+  char_cols <- names(test_data %>% dplyr::select_if(is.character))
+  num_cols <- names(test_data %>% dplyr::select_if(is.numeric))
+
+  for (i in 1:10){
+
+    group_rdm <- sample(char_cols, 4)
+    var_rdm = sample(num_cols, 1)
+
+    test_that("Summary Table Works", {
+      expect_error(preview_sample(data = test_data,
+                                  group_by = group_rdm,
+                                  var = var_rdm,
+                                  stats = c("min", "max", "median", "mean")),
+                   NA)
+    })
+  }
+}
