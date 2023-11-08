74385396fbce361ad84bb68f1a360636b74a6c34
Thiyaghessan <thiyaghessan@gmail.com>
2023-11-08 10:19:39 -0500

    Perform test on a subset of data
    For improved speed and runtime, preview_sample() is run on a random
    sample of columns only 5 times and for only a smaller sample of the
    data. The efficacy of the function is dependent on the columns being
    correct in the summary table and not the number of rows.

--- tests/testthat/test-preview_sample.R
+++ tests/testthat/test-preview_sample.R
@@ -5,12 +5,12 @@ library(dplyr)
 for (i in 1:3){
 
   test_data <- create_test() %>%
-    dplyr::slice_head(n = 1000)
+    dplyr::slice_head(n = 100)
 
   char_cols <- names(test_data %>% dplyr::select_if(is.character))
   num_cols <- names(test_data %>% dplyr::select_if(is.numeric))
 
-  for (i in 1:10){
+  for (i in 1:5){
 
     group_rdm <- sample(char_cols, 4)
     var_rdm = sample(num_cols, 1)
